0fc4e1fc57af83b195c2959633ccffea7d4b75f7
Rafael del Valle <rvalle@husio.org>
2011-12-31 12:18:33 +0100

    created basic project structure and first maven dependencies

---
+++ weather/src/main/java/org/husio/weather/WeatherApplication.java
@@ -0,0 +1,12 @@
+package org.husio.weather;
+
+public class WeatherApplication {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	System.out.println("Hello World from Weather Application");
+    }
+
+}
