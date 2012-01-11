c7f031486ec4c318da0af63133944c68e0345ec9
Rafael del Valle <rvalle@husio.org>
2012-01-11 19:05:29 +0100

    creating PWS url

--- pwsweather/src/main/java/org/husio/weather/service/pws/Driver.java
+++ pwsweather/src/main/java/org/husio/weather/service/pws/Driver.java
@@ -1,13 +1,26 @@
 package org.husio.weather.service.pws;
 
+import java.net.URI;
 import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.measure.quantity.Pressure;
+import javax.measure.quantity.Quantity;
 import javax.measure.quantity.Temperature;
 import javax.measure.quantity.Velocity;
 import javax.measure.unit.Unit;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIUtils;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.message.BasicNameValuePair;
+import org.husio.Configuration;
 import org.husio.api.weather.Humidity;
+import org.husio.api.weather.ObservedWeatherMeasure;
+import org.husio.api.weather.ObservedWeatherMeasure.ENVIRONMENT;
+import org.husio.api.weather.ObservedWeatherMeasure.TYPE;
 import org.husio.api.weather.WeatherCommunityService;
 import org.husio.api.weather.WeatherUnits;
 import org.husio.api.weather.evt.WeatherObservationEvent;
@@ -70,6 +83,10 @@ public class Driver implements WeatherCommunityService{
     public static final Unit<Velocity> PWS_WINDSPEED_UNIT = WeatherUnits.MILES_PER_HOUR;
     
     public static final String PWS_UNKNOWN_VALUE= "NA";
+    
+    private String stationId;
+    private String password;
+    
 
 
     // TODO: Still pending implementation
@@ -87,6 +104,8 @@ public class Driver implements WeatherCommunityService{
     public Driver(){
 	log.info("Starting PSWeather.com Community Service Connector");
 	EventBusService.subscribe(this);
+	this.stationId=Configuration.getProperty(PWS_STATION_ID_CONF_PARAM);
+	this.password=Configuration.getProperty(PWS_PASSWORD_CONF_PARAM);
     }
     
     /**
@@ -94,8 +113,45 @@ public class Driver implements WeatherCommunityService{
      * @param weather
      */
     @EventHandler
-    public void handleWeatherInformation(WeatherObservationEvent weather){
+    public void handleWeatherInformation(WeatherObservationEvent weather) throws Exception{
 	log.debug("Submitting Weather Information:"+weather.getWeatherObservation());
+	List<NameValuePair> qparams = new ArrayList<NameValuePair>();
+
+	// Add basic params
+	qparams.add(new BasicNameValuePair(PWS_ACTION_PARAM, PWS_ACTION_VALUE));
+	qparams.add(new BasicNameValuePair(PWS_STATION_ID_PARAM, this.stationId));
+	qparams.add(new BasicNameValuePair(PWS_PASSWORD_PARAM, this.password));
+	qparams.add(new BasicNameValuePair(PWS_SOFTWARE_PARAM, PWS_SOFTWARE_VALUE));
+	qparams.add(new BasicNameValuePair(PWS_DATE_PARAM, PWS_DATE_FORMAT.format(weather.getWeatherObservation().getTimestamp())));
+
+	// Add each of the weather observations
+	this.addMeasurement(qparams, weather, PWS_HUMIDITY_PARAM, PWS_HUMIDITY_UNIT, ENVIRONMENT.OUTDOOR, TYPE.DISCRETE);
+	this.addMeasurement(qparams, weather, PWS_PRESSURE_PARAM, PWS_PRESSURE_UNIT, ENVIRONMENT.OUTDOOR, TYPE.DISCRETE);
+	this.addMeasurement(qparams, weather, PWS_TEMPERATURE_PARAM, PWS_TEMPERATURE_UNIT, ENVIRONMENT.OUTDOOR, TYPE.DISCRETE);
+	this.addMeasurement(qparams, weather, PWS_WINDSPEED_PARAM, PWS_WINDSPEED_UNIT, ENVIRONMENT.OUTDOOR, TYPE.AVERAGE);
+	this.addMeasurement(qparams, weather, PWS_WINDGUST_PARAM, PWS_WINDGUST_UNIT, ENVIRONMENT.OUTDOOR, TYPE.GUST);
+	
+	// Create the URI
+	URI uri = URIUtils.createURI("http",PWS_SERVER , -1, PWS_UPDATE_URL, 
+	URLEncodedUtils.format(qparams, "UTF-8"), null);
+	HttpGet httpget = new HttpGet(uri);
+	log.debug("The URI is:"+httpget.getURI());   
+    }
+    
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public void addMeasurement(List<NameValuePair> params, WeatherObservationEvent weather, String param, Unit u, ObservedWeatherMeasure.ENVIRONMENT e, ObservedWeatherMeasure.TYPE t ){
+	String value=PWS_UNKNOWN_VALUE;
+	// Get the mesure we are after
+	ObservedWeatherMeasure<? extends Quantity> measure=weather.getWeatherObservation().getMeasures().get(u.getDimension(), e, t);
+	// If the measure exists and it is valid, add it in the required unit, and as float
+	if(measure!=null){
+	    if(measure.isValidMetric()){
+		float v=measure.getMeasure().floatValue(u);
+		value=Float.toString(v);
+	    }
+	}
+	params.add(new BasicNameValuePair(param, value));
     }
 
 }
