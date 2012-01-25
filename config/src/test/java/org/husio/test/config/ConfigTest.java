c147c650fc631ea74024ee125cdb73664ca49218
Rafael del Valle <rvalle@husio.org>
2012-01-25 08:23:17 +0100

    added a test to ensure boolean config options work properly

---
+++ config/src/test/java/org/husio/test/config/ConfigTest.java
@@ -0,0 +1,20 @@
+package org.husio.test.config;
+
+import org.husio.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+public class ConfigTest {
+
+    private static final Logger log = LoggerFactory.getLogger(ConfigTest.class);
+    
+    @Test
+    public void testBooleanConfigOption(){
+	log.debug("Testing boolean configuration options");
+	String testProperty="org.husio.weather.station.wh1080.Driver.usbForceClaim";
+	boolean t=Configuration.getBooleanProperty(testProperty);
+	assert t: "Option not found or not true";
+    }
+
+}
